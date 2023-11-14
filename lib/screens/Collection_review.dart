11c37534d5ffed96b631b93018f46d839099a100
KustyBoo <kpananda77@gmail.com>
2023-11-15 00:13:05 +0800

    Menambahkan collection review page

---
+++ lib/screens/Collection_review.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CollectionReviewPage extends StatefulWidget {
+  const CollectionReviewPage({super.key});
+
+  @override
+  State<CollectionReviewPage> createState() => _CollectionReviewPageState();
+}
+
+class _CollectionReviewPageState extends State<CollectionReviewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
